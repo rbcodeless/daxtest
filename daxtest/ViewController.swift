03cf31d21e10bed0ea7d3e529ff9bd0e971fe517
rinus bester <rinusbester@icloud.com>
2017-06-05 22:02:49 +0200

    commit no1

--- daxtest/ViewController.swift
+++ daxtest/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thelabel: UILabel!
+    
+    
+    @IBAction func DRUKKNOP(_ sender: Any) {
+    thelabel.text = "MIDDAG MENSE"
+    }
+    
+    
+    
+    @IBAction func DRUKSKOON(_ sender: Any) {
+        thelabel.text = "SKOON"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+        thelabel.text = "Robocop"
     }
 
     override func didReceiveMemoryWarning() {
